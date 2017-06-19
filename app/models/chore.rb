e173f9145e4bb098378b7c078704b4ed78ad0066
Zenia Villa <zeniavilla@hotmail.com>
2017-06-19 16:44:59 -0700

    set category default for chore

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,4 +1,12 @@
 class Chore < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  before_create :set_category_default
+
+  private
+
+  def set_category_default
+    self.category ||= "uncategorized"
+  end
 end
