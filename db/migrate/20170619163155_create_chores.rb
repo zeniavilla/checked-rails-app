715c0e70098b4004e571942a4a611425829a6f93
Zenia Villa <zeniavilla@hotmail.com>
2017-06-19 09:52:42 -0700

    update chore migration with foreign keys

--- db/migrate/20170619163155_create_chores.rb
+++ db/migrate/20170619163155_create_chores.rb
@@ -7,7 +7,8 @@ class CreateChores < ActiveRecord::Migration[5.1]
       t.string :frequency_interval
       t.integer :duration_repetition
       t.datetime :duration_end_date
-      t.belongs_to :chore, foreign_key: true
+      t.belongs_to :user, foreign_key: true
+      t.belongs_to :category, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -26,10 +26,12 @@ ActiveRecord::Schema.define(version: 20170619163416) do
     t.string "frequency_interval"
     t.integer "duration_repetition"
     t.datetime "duration_end_date"
-    t.integer "chore_id"
+    t.integer "user_id"
+    t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["chore_id"], name: "index_chores_on_chore_id"
+    t.index ["category_id"], name: "index_chores_on_category_id"
+    t.index ["user_id"], name: "index_chores_on_user_id"
   end
 
   create_table "teams", force: :cascade do |t|
