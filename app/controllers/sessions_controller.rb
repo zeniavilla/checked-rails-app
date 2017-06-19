a14f8cd806d0132b32bad1a7beafd97a80b2a349
Zenia Villa <zeniavilla@hotmail.com>
2017-06-19 15:29:13 -0700

    add #new action to sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,23 @@
 class SessionsController < ApplicationController
     def new
-        @session = Session.new
+        
+    end
+
+    def create
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            flash[:success] = "Successfully signed in."
+            redirect_to root_path
+        else
+            flash[:error] = "The email and password combination could not be found."
+            render :new
+        end
     end
 
     def destroy
         session.delete :user_id
+        flash[:success] = "Successfully signed out."
         redirect_to root_path
     end
 end
