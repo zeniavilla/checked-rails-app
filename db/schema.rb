cedd21292ef9aace528c3b4df35bbc39a235e95c
Zenia Villa <zeniavilla@hotmail.com>
2017-06-19 09:35:16 -0700

    add team model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170619163155) do
+ActiveRecord::Schema.define(version: 20170619163416) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(version: 20170619163155) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["chore_id"], name: "index_chores_on_chore_id"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
