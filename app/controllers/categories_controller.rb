09b40ee881b951445fd17bcf2582dd13a29d229c
Zenia Villa <zeniavilla@hotmail.com>
2017-06-19 16:27:36 -0700

    add create action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,26 @@
 class CategoriesController < ApplicationController
+    before_action :set_category, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @categories = Category.all
+    end
+
     def show
     end
 
     def new
+        @category = Category.new
     end
 
     def create
+        @category = Category.new(category_params)
+        if @category.save
+            flash[:success] = "Category successfully created"
+            redirect_to categories_path
+        else
+            # flash[:error] = "Title can't be blank."
+            render :new
+        end
     end
 
     def edit
@@ -15,5 +30,15 @@ class CategoriesController < ApplicationController
     end
 
     def destroy
+    end
+
+    private
+
+    def set_category
+        @category = Category.find_by(id: params[:id])
+    end
+
+    def category_params
+        params.require(:category).permit(:title)
     end
 end
