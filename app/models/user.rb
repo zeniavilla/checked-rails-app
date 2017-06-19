16edec83b30f7145a04745faf40bfea6a8b2a5d6
Zenia Villa <zeniavilla@hotmail.com>
2017-06-19 15:13:13 -0700

    add validation for password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
+  attr_accessor :password_confirmation
+  
   belongs_to :team, required: false
   has_many :chores
   has_many :categories, through: :chores
+
+  has_secure_password
+  validates_confirmation_of :password
 
   def team_attributes=(team_attributes)
     team = Team.find_or_create_by(team_attributes)
