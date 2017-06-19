63632dd7bd72186f87385dd4364d5996eb408478
Zenia Villa <zeniavilla@hotmail.com>
2017-06-19 14:12:33 -0700

    add new route for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,8 @@
 class SessionsController < ApplicationController
+    def new
+        @session = Session.new
+    end
+
     def destroy
         session.delete :user_id
         redirect_to root_path
