2defc870f0a0a1dbaae18e009d8f38c1cde820d2
Zenia Villa <zeniavilla@hotmail.com>
2017-06-19 12:46:46 -0700

    update current_user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,6 @@ module ApplicationHelper
     end
 
     def current_user
-        @current_user ||= User.find_by(id: session[:user_id]
+        @current_user ||= User.find_by(id: session[:user_id])
     end
 end
