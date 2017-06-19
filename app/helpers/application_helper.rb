18411c234e14f3c0c4741f4e44888b7582d2ebc0
Zenia Villa <zeniavilla@hotmail.com>
2017-06-19 12:38:12 -0700

    add signed in? and current user methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def user_signed_in?
+        !!session[:user_id]
+    end
+
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id]
+    end
 end
