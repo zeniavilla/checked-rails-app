273598f33cf6c6bb856c729a21ae2e6cafdde161
Zenia Villa <zeniavilla@hotmail.com>
2017-06-20 09:38:42 -0700

    move current_user to application controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,6 @@ module ApplicationHelper
         !!session[:user_id]
     end
 
-    def current_user
-        @current_user ||= User.find_by(id: session[:user_id])
-    end
-
     def current_team
         current_user.team
     end
