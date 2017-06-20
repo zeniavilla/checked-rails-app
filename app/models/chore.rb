6cb89f953a76334e8e027ec2964fa0c3ed9807cf
Zenia Villa <zeniavilla@hotmail.com>
2017-06-20 10:17:23 -0700

    fix chore defaults and add conditionals for blank category fields

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -2,15 +2,17 @@ class Chore < ApplicationRecord
   belongs_to :user, required: false
   belongs_to :category, required: false
 
-  before_create :set_defaults
+  after_initialize :set_defaults
 
   validates :title, presence: true
   validates :frequency_interval, presence: false, unless: :frequency_amount?
 
   def category_attributes=(category_attributes)
-    category = Category.find_or_create_by(category_attributes)
-    self.category=(category)
-    self.save
+    if !category_attributes.values.first.blank?
+      category = Category.find_or_create_by(category_attributes)
+      self.category=(category)
+      self.save
+    end
   end
 
   private
