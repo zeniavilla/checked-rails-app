529b4a89908dac4f7842f4bb8cc3c84ad4d41efc
Zenia Villa <zeniavilla@hotmail.com>
2017-06-20 08:33:11 -0700

    add current_team method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
     def current_user
         @current_user ||= User.find_by(id: session[:user_id])
     end
+
+    def current_team
+        current_user.team
+    end
 end
