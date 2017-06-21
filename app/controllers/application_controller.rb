64ccfb51116c1ef069fd2b4a172de4d1e3fe15da
Zenia Villa <zeniavilla@hotmail.com>
2017-06-21 09:17:19 -0700

    place current_team method in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user
+  helper_method :current_user, :current_team
   
   def current_user
         @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def current_team
+      current_user.team
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,4 @@ module ApplicationHelper
     def user_signed_in?
         !!session[:user_id]
     end
-
-    def current_team
-        current_user.team
-    end
 end
