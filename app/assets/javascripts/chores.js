32a84aca1bc17c4ec06c377fda1c41bb1d1f0fa8
Zenia <zeniavilla@hotmail.com>
2017-07-20 14:58:28 -0700

    add Chore attributes to js

--- app/assets/javascripts/chores.js
+++ app/assets/javascripts/chores.js
@@ -1,16 +1,19 @@
 function Chore(attributes) {
         this.title = attributes.title;
         this.id = attributes.id;
-        this.frequency_amount = attributes.frequency_amount;
-        this.frequency_interval = attributes.frequency_interval;
-        this.duration_end_date = attributes.duration_end_date;
+        this.frequencyAmount = attributes.frequency_amount;
+        this.frequencyInterval = attributes.frequency_interval;
+        this.durationEndDate = attributes.duration_end_date;
+        this.userName = attributes.user.name;
+        this.categoryId = attributes.category.id;
+        this.categoryTitle = attributes.category.title;
 }
 
 $(function() {
     Chore.templateSource = $('#chore-template').html();
     Chore.template = Handlebars.compile(Chore.templateSource);
 
-    Chore.template({title: 'Chore'})
+    Chore.template({title: 'Chore', categoryTitle: 'Cool', userName: 'Steve Jobs', durationEndDate: '2017-07-28T00:00:00.000Z', frequencyAmount: 2, frequencyInterval: 'days'})
 
     Chore.prototype.renderTd = function() {
         return Chore.template(this);
@@ -38,9 +41,10 @@ $(function() {
         })
         .done(function(json) {
             var chore = new Chore(json);
+            debugger
             var choreTd = chore.renderTd();
 
-            $('table tr').append(choreTd)
+            $('table').append(choreTd)
         });
     });
 });
