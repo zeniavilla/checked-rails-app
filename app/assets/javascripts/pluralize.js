38c5cb061e9a911a573089e90d97ae61b3e82b50
Zenia <zeniavilla@hotmail.com>
2017-07-22 10:58:28 -0700

    move pluralize helper into own js fille

---
+++ app/assets/javascripts/pluralize.js
@@ -0,0 +1,7 @@
+Handlebars.registerHelper('pluralize', function(number, word) {
+    if (number === 1) { 
+        return `${number} ${word.replace('s', '')}`; 
+    } else {
+        return `${number} ${word}`;
+    }
+});
