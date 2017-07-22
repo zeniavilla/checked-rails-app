292c89f3fe826b8b4bf131c852fd8539aca3c173
Zenia <zeniavilla@hotmail.com>
2017-07-22 10:59:22 -0700

    create ajax call and render json in handlebars template for user links on team#show view

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -1,5 +1,22 @@
-$(function() {
-    $('.js-user-link').on('click', function(event) {
-        event.preventDefault();
-    })
-});
+$(function(){
+        $(".js-user-link").on('click', function(e) {
+            e.preventDefault()
+            var path = this.href;
+
+            $.ajax({
+                url: path,
+                dataType: 'json',
+                method: 'GET'
+            })
+            .success(function(json) {
+                var user = new User(json);
+
+                var userLi = user.renderLi();
+
+                $('.js-user-info-' + user.id).append(userLi);
+            })
+            .error(function(response) {
+                console.log("Broken", response)
+            })
+        })
+    });
