ebeb84a497912a5a21e9d9eaad32844c9e08da20
Zenia <zeniavilla@hotmail.com>
2017-07-22 09:44:03 -0700

    add updated_at field to chore serializer

--- app/serializers/chore_serializer.rb
+++ app/serializers/chore_serializer.rb
@@ -1,5 +1,5 @@
 class ChoreSerializer < ActiveModel::Serializer
-  attributes :id, :title, :active, :frequency_amount, :frequency_interval, :duration_end_date
+  attributes :id, :title, :active, :frequency_amount, :frequency_interval, :duration_end_date, :updated_at
   belongs_to :category
   belongs_to :user
 end
