b3f62283bfd81ec6ed4a3dd83c7d19a961c82850
Zenia <zeniavilla@hotmail.com>
2017-08-03 18:48:00 -0700

    add ajax delete request to categories.js

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,10 +1,13 @@
+'use strict'
+
 $(function() {
     viewAllCategories();
 });
 
-var viewAllCategories = () => {
+const viewAllCategories = () => {
     $('.js-all-categories').on('click', function(event) {
         event.preventDefault();
+        
         var path = event['currentTarget']['href']
         
         $.ajax({
@@ -16,7 +19,31 @@ var viewAllCategories = () => {
             json.forEach(Chore.success);
 
             $('.js-chores-title').text('All Chores')
+            addDeleteChoreIconListeners();
         })
         .error(Chore.error);
     });
+}
+
+const addDeleteChoreIconListeners = () => {
+    $('button#delete_chore_icon').on('click', function(event) {
+        event.preventDefault();
+        
+        var method = this.dataset.method;
+        var path = this.dataset.url;
+        var delete_link_row = $(this).parents('tr')
+
+        $.ajax ({
+            url: path,
+            method: 'DELETE',
+        })
+        .success(function(response) {
+            delete_link_row.remove();
+        })
+        .error(function(response) {
+            console.log("Error", response)
+        })
+        
+
+    })
 }
