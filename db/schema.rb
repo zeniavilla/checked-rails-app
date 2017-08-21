35684960cc120c4675dec3686c02054f173350e3
Zenia <zeniavilla@hotmail.com>
2017-08-21 11:14:21 -0700

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170626210807) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string "title"
     t.string "color"
@@ -26,8 +29,8 @@ ActiveRecord::Schema.define(version: 20170626210807) do
     t.string "frequency_interval"
     t.integer "duration_repetition"
     t.datetime "duration_end_date"
-    t.integer "user_id"
-    t.integer "category_id"
+    t.bigint "user_id"
+    t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_chores_on_category_id"
@@ -41,7 +44,7 @@ ActiveRecord::Schema.define(version: 20170626210807) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.integer "team_id"
+    t.bigint "team_id"
     t.string "name"
     t.string "email"
     t.string "password_digest"
@@ -50,4 +53,6 @@ ActiveRecord::Schema.define(version: 20170626210807) do
     t.index ["team_id"], name: "index_users_on_team_id"
   end
 
+  add_foreign_key "chores", "categories"
+  add_foreign_key "chores", "users"
 end
