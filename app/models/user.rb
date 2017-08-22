db4b39d98b5abaf188a57e2dd7e4f44dd58a3a4c
Zenia <zeniavilla@hotmail.com>
2017-08-22 10:59:12 -0700

    revise find_or_create_by_omniauth to use provider and uid

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,8 +23,9 @@ class User < ApplicationRecord
   end
 
   def self.find_or_create_by_omniauth(auth_hash)
-    self.where(email: auth_hash["info"]["email"]).first_or_create do |user|
-      user.name = auth_hash["info"]["name"]
+    where(provider: auth_hash.provider, uid: auth_hash.uid).first_or_create do |user|
+      user.email = auth_hash.info.email
+      user.name = auth_hash.info.name
       user.password = SecureRandom.hex
       user.password_confirmation = user.password
     end
