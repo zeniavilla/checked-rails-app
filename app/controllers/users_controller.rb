42b4c9739c0e7046bdb3c087fe237f6afd093575
Zenia <zeniavilla@hotmail.com>
2017-08-22 19:04:47 -0700

    change users#create redirect to profile path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
         if @user.save
             session[:user_id] = @user.id
             flash[:success] = "Successfully signed up."
-            redirect_to chores_path
+            redirect_to edit_profile_path(@user)
         else
             render :new
         end
